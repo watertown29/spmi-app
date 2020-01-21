831340b87591d1d163fe23b305c57035b7d7b483
Daniel Pati <djp297@cornell.edu>
2020-01-21 17:21:55 -0500

    fix sidebar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import { FaBars, FaThLarge} from "react-icons/fa";
+import { FaBars, FaThLarge, FaClock, FaCommentAlt, FaAngry } from "react-icons/fa";
 import { IoIosArrowBack} from "react-icons/io";
 
 export default class Navbar extends Component {
@@ -14,33 +14,33 @@ export default class Navbar extends Component {
         return (
             <>
                 <nav className="header">
-                    <span className="hamburger" onClick={this.handleToggle}>{<FaBars size={60}/>}</span>
+                    <span onClick={this.handleToggle}>{<FaBars size={40} className="hamburger"/>}</span>
                     <h1 className="header-title">SPMI Home Dashboard</h1>
                 </nav>
-                
-                    <div className={this.state.isOpen?"menu show-nav":"menu"}>
+                <div className={this.state.isOpen?"menu show-nav":"menu"}>
                         <ul>
                             <li>
                                 <FaThLarge className="menu-icon"/>
                                 <Link className="menu-titles" to="/">Dashboard</Link>
                             </li>
                             <li >
-                                <FaThLarge className="menu-icon"/>
-                                <Link className="menu-titles" to="/Content">Content</Link>
-                            </li>
-                            <li >
-                                <FaThLarge className="menu-icon"/>
+                                <FaClock className="menu-icon"/>
                                 <Link className="menu-titles" to="/Syllabus">Syllabus</Link>
                             </li>
                             <li >
-                                <FaThLarge className="menu-icon"/>
+                                <FaCommentAlt className="menu-icon"/>
+                                <Link className="menu-titles" to="/Content">Content</Link>
+                            </li>
+                            
+                            <li >
+                                <FaAngry className="menu-icon"/>
                                 <Link className="menu-titles" to="/err">Error</Link>
                             </li >
                             <div className="back-btn-container" onClick={this.handleToggle}>
-                                {<IoIosArrowBack size={75} className="back-btn"/>}
+                                {<IoIosArrowBack size={50} className="back-btn"/>}
                             </div>
                         </ul>    
-                    </div>
+                </div>    
                 
             </>
         )
