436adba26224b2e1a65b9457819abd2dd15ed1f9
Daniel Pati <djp297@cornell.edu>
2020-01-21 15:43:35 -0500

    redo sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import { FaAlignJustify} from "react-icons/fa";
+import { FaBars, FaThLarge} from "react-icons/fa";
 import { IoIosArrowBack} from "react-icons/io";
 
 export default class Navbar extends Component {
@@ -14,28 +14,34 @@ export default class Navbar extends Component {
         return (
             <>
                 <nav className="header">
-                    <span className="hamburger" onClick={this.handleToggle}>{<FaAlignJustify size={60}/>}</span>
-                    <h1 className="header-title">Dashboard</h1>
+                    <span className="hamburger" onClick={this.handleToggle}>{<FaBars size={60}/>}</span>
+                    <h1 className="header-title">SPMI Home Dashboard</h1>
                 </nav>
-                <div>
-                    <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
-                        <li>
-                            <Link to="/">Dashboard</Link>
-                        </li>
-                        <li>
-                            <Link to="/Content">Content</Link>
-                        </li>
-                        <li>
-                            <Link to="/Syllabus">Syllabus</Link>
-                        </li>
-                        <li>
-                            <Link to="/err">Error</Link>
-                        </li>
-                        <div className="back-btn-container" onClick={this.handleToggle}>
-                            {<IoIosArrowBack size={75} className="back-btn"/>}
-                        </div>
-                    </ul>    
-                </div>
+                
+                    <div className={this.state.isOpen?"menu show-nav":"menu"}>
+                        <ul>
+                            <li>
+                                <FaThLarge className="menu-icon"/>
+                                <Link className="menu-titles" to="/">Dashboard</Link>
+                            </li>
+                            <li >
+                                <FaThLarge className="menu-icon"/>
+                                <Link className="menu-titles" to="/Content">Content</Link>
+                            </li>
+                            <li >
+                                <FaThLarge className="menu-icon"/>
+                                <Link className="menu-titles" to="/Syllabus">Syllabus</Link>
+                            </li>
+                            <li >
+                                <FaThLarge className="menu-icon"/>
+                                <Link className="menu-titles" to="/err">Error</Link>
+                            </li >
+                            <div className="back-btn-container" onClick={this.handleToggle}>
+                                {<IoIosArrowBack size={75} className="back-btn"/>}
+                            </div>
+                        </ul>    
+                    </div>
+                
             </>
         )
     }
